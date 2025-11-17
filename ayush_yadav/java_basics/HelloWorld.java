23fa6466797aa2445dab60dff1b70bf01f2d225e
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-11-18 05:07:16 +0530

    we learn second program

--- ayush_yadav/java_basics/HelloWorld.java
+++ ayush_yadav/java_basics/HelloWorld.java
@@ -1,7 +1,6 @@
-public class HelloWorld{
-    public static void main (String[]args) {
+public class HelloWorld {
+    public static void main (String[]args){
         String name="Ayush";
         System.out.println(name);
-    }
-}
+}}
         
---
+++ ayush_yadav/java_basics/StringExample.java
@@ -0,0 +1,28 @@
+public class StringExample{
+    public static void main (String[]args){
+        String name="Ayush";
+        System.out.println("My name is Ayush");
+
+  System.out.println("My name is Ayush");
+
+  System.out.println("My name is Ayush");
+
+  System.out.println("My name is Ayush");
+
+  System.out.println("My name is Ayush");
+
+  System.out.println("My name is Ayush");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
