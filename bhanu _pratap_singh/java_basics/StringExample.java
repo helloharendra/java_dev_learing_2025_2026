6231e7441f1d8d35eba1388d1ecb306969ed8418
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-11-18 17:06:25 +0530

    learn second program

---
+++ bhanu _pratap_singh/java_basics/StringExample.java
@@ -0,0 +1,11 @@
+public class StringExample{
+    public static void main(String[]args){
+        String name="harendra";
+        System.out.println ("my name is"+name);
+        System.out.println ("my name is"+name);
+        System.out.println ("my name is"+name);
+        System.out.println ("my name is"+name);
+        System.out.println ("my name is"+name);
+        System.out.println("my name is"+ name);
+    }
+}
