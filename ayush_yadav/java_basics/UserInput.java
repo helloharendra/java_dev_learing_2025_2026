8cf454af13438ff97396d64698b0a1381335a202
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-11-28 16:34:09 +0530

    Addition of two numbers using scanner class

---
+++ ayush_yadav/java_basics/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main (String[]args){
+Scanner SC=new Scanner(System.in);
+System.out.println("Enter Your name");
+String name =SC.nextLine();
+int a = 10;
+int b = 20; 
+int c = a+b;
+System.out.println("Name is :" + name);
+    }  
+}
