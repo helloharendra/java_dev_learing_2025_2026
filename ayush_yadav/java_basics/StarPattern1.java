ca805c849dce7095823387134b44ef2b6036b4a0
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-11 15:48:27 +0530

    StarPattern program

--- ayush_yadav/java_basics/StarPattern1.java
+++ ayush_yadav/java_basics/StarPattern1.java
@@ -1,16 +1,14 @@
 public class StarPattern1{
-public static void main (String[]args){
-    int n = 11
-    ;
-    System.out.println("SimpleTrianglePattern");
-    for (int i = 0; i < 30; i++) {
-     for (int j = 0; j < 8
-        ; j++) {
-     System.out.print("*");  
-     }   
-     System.out.println();
+    public static void main (String[]args){
+        int n = 15;
+        System.out.println("SimpleTrainglePattern");
+       for (int i = 0; i < 10; i++) {
+       for (int j = 0; j < 10; j++) {
+       System.out.print ("*");    
+       }
+
+       }
+       System.out.println();
+            
+        }
     }
-
-}
-}
-
