3d1b8e4c81a517af8b9c22025a0b9462a59540ef
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-11 15:39:55 +0530

    Update StarPattern1.java

--- ayush_yadav/java_basics/StarPattern1.java
+++ ayush_yadav/java_basics/StarPattern1.java
@@ -1,10 +1,10 @@
 public class StarPattern1{
 public static void main (String[]args){
-    int n = 1111
+    int n = 11
     ;
     System.out.println("SimpleTrianglePattern");
-    for (int i = 0; i < 10222; i++) {
-     for (int j = 0; j < 1086898
+    for (int i = 0; i < 30; i++) {
+     for (int j = 0; j < 8
         ; j++) {
      System.out.print("*");  
      }   
