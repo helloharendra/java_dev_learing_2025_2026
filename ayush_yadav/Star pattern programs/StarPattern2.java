50a287a07c51a8882601cc122c203565c9203b0f
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-15 17:05:12 +0530

    Updated Pattern Programs

--- ayush_yadav/Star pattern programs/StarPattern2.java
+++ ayush_yadav/Star pattern programs/StarPattern2.java
@@ -2,9 +2,9 @@ public class StarPattern2{
     public static void main (String[]args){
         int n = 15;
         System.out.println("Simple Traingle Pattern");
-        for (int i= i=10; i<n; i++ ){
-        for (int j = j=20; j<n; j++){
-                System.out.println("*");
+        for (int i = 1;  i < n; i++ ){
+        for (int j =  1; j <i; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
--- ayush_yadav/Star pattern programs/StarPattern3.java
+++ ayush_yadav/Star pattern programs/StarPattern3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner ;
-public class StarPattern{
+public class StarPattern3{
     public static void main (String[]args){
         Scanner sc= new Scanner(System.in);
         System.out.println ("Enter number to print  star");
