30bd8a312aaf96b7a7f9370ded04504d5d84a899
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-15 17:05:27 +0530

    Updated Pattern Programs.

--- ayush_yadav/java_basics/UserInput.java
+++ ayush_yadav/java_basics/UserInput.java
@@ -1,5 +1,5 @@
 
-   public Class SumOfNumberScanner{
+   public Class UserInput{
     public  static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Addition Of two Numbers");
