8a3080453572d167f1cda7ddfdb0714c9560ba17
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-16 16:36:54 +0530

    Squareprogram and SameNumberProgram

--- ayush_yadav/Star pattern programs/StarPattern2.java
+++ ayush_yadav/Star pattern programs/StarPattern2.java
@@ -1,12 +1,9 @@
 public class StarPattern2{
-    public static void main (String[]args){
-        int n = 15;
-        System.out.println("Simple Traingle Pattern");
-        for (int i = 1;  i < n; i++ ){
-        for (int j =  1; j <i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+public static void main (String []args){
+    int n = 10;
+    System.out.println("Simple traingular Pattern");
 }
+
+}
+
+
--- ayush_yadav/Star pattern programs/StarPattern3.java
+++ ayush_yadav/Star pattern programs/StarPattern3.java
@@ -1,19 +1,5 @@
-import java.util.Scanner ;
-public class StarPattern3{
-    public static void main (String[]args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println ("Enter number to print  star");
-        int n =sc.nextInt();
-        System.out.println("SimpleTrainglePattern");
-        for (int i = 1; i <=n; i++) {
-        for (int j = 1; j<=i; j++){
-
-               System.out.print ("*");
-        }
-          System.out.println();
-
-        
-        }
-     
-    }
+public class StarPattern3 {
+  public static void main (String []args){
+    
+  }
 }
