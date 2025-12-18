a4fa0c152b397047898ef72e12f082ed70970fbe
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-12-18 16:42:08 +0530

    inverted right traingle

--- ayush_yadav/Star pattern programs/SameNumberTraingle.java
+++ ayush_yadav/Star pattern programs/SameNumberTraingle.java
@@ -4,7 +4,7 @@ public class SameNumberTraingle{
         int n = 5;
         for (int i = 1; i<= n; i++){
         for (int j = 1; j<=n; j++){
-        System.out.print(i + "");
+        System.out.print( i +"");
         }    
         System.out.println();
         }
--- ayush_yadav/Star pattern programs/StarPattern1.java
+++ ayush_yadav/Star pattern programs/StarPattern1.java
@@ -1,9 +1,12 @@
-public class StarPattern1 {
-public static void main (String[]args){
+public class StarPattern1{
+    public static void main(String []args){
+        int n = 5;
+        for (int i = 1; i<= n; i++){
+        for (int j = 1; j <=i; j++){
+            System.out.print("*");
+        }    System.out.println();
 
-    for (int i = 1; i <= 5; i++){
-       System.out.print("*"); 
+        }
+      
     }
-
-}    
 }
