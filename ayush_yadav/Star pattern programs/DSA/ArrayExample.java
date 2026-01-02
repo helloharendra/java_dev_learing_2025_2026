dfe023646f50bdea5a83ed05a65a9cd71395f56e
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-01-02 16:22:09 +0530

    ArrayExample

---
+++ ayush_yadav/Star pattern programs/DSA/ArrayExample.java
@@ -0,0 +1,8 @@
+public class ArrayExample{
+    public static void main (String[]args){
+    int [] Ayush= {10,20,30,40,50,60,70};
+
+    System.out.println(Ayush[6] );
+    System.out.print("Last elements of array" + (Ayush[Ayush.length-1]));
+    }
+}
