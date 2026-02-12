5aaef25a18fe340d66d7a850f0ba31f46832f473
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-02-13 05:22:51 +0530

    ParameterisedFunction

---
+++ ayush_yadav/Star pattern programs/DSA/ParameterisedFunction.java
@@ -0,0 +1,22 @@
+public class ParameterisedFunction {
+
+    void sum () {
+     int a = 10;
+     int b = 20;
+     int c = a+b;
+     System.out.println("Sum is " + c);
+    }
+    void minus () {
+     int a = 10;
+     int b = 20;
+     int c = a-b;
+      System.out.println("Minus is " + c);
+    }   
+    public static void main (String[]args){
+        ParameterisedFunction ayush = new ParameterisedFunction ();
+        ayush.minus();
+        ayush.sum();
+    }
+    }
+
+
--- ayush_yadav/Star pattern programs/DSA/WeeklyCalander.java
+++ ayush_yadav/Star pattern programs/DSA/WeeklyCalander.java
@@ -1,20 +1,20 @@
 public class WeeklyCalander {
   public static void main(String[]args){
-    String day = "MONDAY";
+    String day = "SUNDAY";
     switch (day) {
     case "MONDAY":
     case "TUESDAY":
-     case "WEDNESDAY":
+    case "WEDNESDAY":
     case "THURSDAY":
-      case "FRIDAY":
-        System.out.println("Weekday");
-        break;
-        case "SATURDAY":
-        case "SUNDAY" :
-        System.out.println("Weekday");  
-        break;
-        default:   
-        System.out.println("Invalid Day");  
+    case "FRIDAY":
+    System.out.println("Weekday");
+    break;
+   case "SATURDAY":
+    case "SUNDAY" :
+    System.out.println("Weekday");  
+    break;
+    default:   
+    System.out.println("Invalid Day");  
   }  
 }
 }
