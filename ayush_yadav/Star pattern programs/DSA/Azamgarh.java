d73626c366e5c24986abfbdf2559afc5f373e653
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-02-16 05:14:37 +0530

    Update Azamgarh.java

--- ayush_yadav/Star pattern programs/DSA/Azamgarh.java
+++ ayush_yadav/Star pattern programs/DSA/Azamgarh.java
@@ -1,16 +1,23 @@
+import java.util.Arrays;
+
 public class Azamgarh{
     void Katohi () {
         String pardhan = " Ram" ;
-        int population = 1000;
+        String[]peoples = {"ram", "shayam", "vivek", "ramakant", "vishundev"};
+       int population = peoples.length;
+       
         System.out.println("Pardhan of Katohi" + pardhan);
         System.out.println("Population of Katohi " + population); 
+        System.out.println("Peoples of katohi: "+Arrays.toString(peoples));
     }
 
     void Etori () {
         String pardhan = " Shayam " ;
-        int population = 500;
+        String[]peoples = {"ram", "shayam", "vivek", "ramakant", "vishundev"};
+        int population = peoples.length;
         System.out.println("Pardhan of Etori" + pardhan);
         System.out.println("Population of Etori " + population);
+        System.out.println("Peoples of katohi: "+Arrays.toString(peoples));
 
     }
 
@@ -18,7 +25,7 @@ public class Azamgarh{
         public static void main (String[]args){
             Azamgarh obj = new Azamgarh ();
             obj.Katohi ();
-            obj.Etori  ();
+            obj.Etori ();
         }
 
     }
