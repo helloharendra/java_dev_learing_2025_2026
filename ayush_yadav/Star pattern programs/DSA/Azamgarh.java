e5b8edfab036d38a14dd24635e15dfc5b43353bd
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-02-17 05:36:29 +0530

    program

--- ayush_yadav/Star pattern programs/DSA/Azamgarh.java
+++ ayush_yadav/Star pattern programs/DSA/Azamgarh.java
@@ -1,31 +1,90 @@
 import java.util.Arrays;
 
 public class Azamgarh{
-    void Katohi () {
-        String pardhan = " Ram" ;
-        String[]peoples = {"ram", "shayam", "vivek", "ramakant", "vishundev"};
-       int population = peoples.length;
-       
-        System.out.println("Pardhan of Katohi" + pardhan);
-        System.out.println("Population of Katohi " + population); 
-        System.out.println("Peoples of katohi: "+Arrays.toString(peoples));
+
+
+    static class  Person {
+        String name;
+        String age;
+        String gender;
+        boolean isVoterEligible;
+
+       public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.isVoterEligible = age >= 18;
+       }
+       @override
+       public String toString(){
+        return String.format(" -%s (Age: %d, Gender: %s, Voter: %s)";
+        name, age, gender, is VoterEligible ? "Yes" : "No");
+       }
     }
-
-    void Etori () {
-        String pardhan = " Shayam " ;
-        String[]peoples = {"ram", "shayam", "vivek", "ramakant", "vishundev"};
-        int population = peoples.length;
-        System.out.println("Pardhan of Etori" + pardhan);
-        System.out.println("Population of Etori " + population);
-        System.out.println("Peoples of katohi: "+Arrays.toString(peoples));
-
-    }
-
-
-        public static void main (String[]args){
-            Azamgarh obj = new Azamgarh ();
-            obj.Katohi ();
-            obj.Etori ();
+    static class Villages{
+        String name;
+        String pardhan;
+        List<Person> people;
+        Map<String, Integer> statistics;
+        public Villages (String name, String pardhan){
+        this.name = name;
+        this.pardhan = pardhan;
+        this.people = new  ArrayList<>();
+        this.statistics = new HasMap<>();
         }
+        public void addPerson (String name, int age, String gender){
+            people.add(new Person(name, age, gender));
+        }
+        public void calculatesstatistics () {
+            int totalPopulation = people.size();
+            int males = 0; females = 0; others = 0;
+            int childern = 0; adults = 0; seniorcitizens = 0;
+            int VoterEligible = 0;
+            int maleVoters = 0, femalesVoters = 0;
 
+            for (Person p : people){
+                switch (p.gender.toLowerCase()) {
+                    case "male":
+                        males++;
+                        if(p.isVoterEligible) maleVoters++;
+                        break;
+                        case "female":
+                            females++;
+                            if (p.isVoterEligible) femalesVoters++;
+                            break;
+                            default;
+                            others++;
+                }
+                if (p.age < 18) {
+                    childern ++;
+                }else if (p.age >= 18 && p.age < 60) {
+                    adults++;
+                }else { 
+                    seniorcitizens++;
+                }
+
+                if(p.isVoterEligible){
+                    eligibleVoters++;
+                }
+                }
+                statistics.put("Total Population",totalPopulation);
+                statistics.put("Males",males);
+                statistics.put("Females",females);
+                statistics.put("Others"others);
+                statistics.put("Children (Below 18)",childern);
+                statistics.put("Adults (18-59)",adults);
+                statistics.put("Senior Citizens (60+)",seniorcitizens);
+                statistics.put("Eligible Voters",eligibleVoters);
+                statistics.put("Male Voters",maleVoters);
+                statistics.put("Female Voters",femalesVoters);
+            }
+            public void displayVillageDetails() {
+                System.out.println("\n=================================");
+                System.out.println("VILLAGE: + name.toUpperCase()");
+                System.out.println("\=================================");
+                System.out.println ("Pardhan/Sarpanch: " + pardhan); 
+                System.out.println ("Total Population: " + peoples.size());
+            }
+        }
     }
+
